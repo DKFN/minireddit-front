a4d5685904358af3d3b56a75811e07c07cb03a51
DKFN <valere.tetelin@gmail.com>
2019-03-05 11:31:19 +0100

    Dependency install on startup script

--- fullaunch.sh
+++ fullaunch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 docker build -t reddit-frontend ./docker-reddit-front
+docker run -v $(pwd):/data -p 3000:3000 redditfront install
 docker run -v $(pwd):/data -p 3000:3000 redditfront
 
