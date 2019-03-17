b722533fe0c56c9f75b732b6a2751a6802b7e383
DKFN <valere.tetelin@gmail.com>
2019-03-17 15:59:36 +0100

    Added build configuration API URL and added prodbuild.sh

---
+++ prodbuild.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+docker build -t reddit-frontend ./docker-reddit-front
+docker run -v $(pwd):/data -p 3000:3000 reddit-frontend install
+docker run -v $(pwd):/data -p 3000:3000 reddit-frontend run-script build http://reddit-backend.tetel.in/
+
