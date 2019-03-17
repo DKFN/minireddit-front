0e6e482a38ccc601d1fef2b4e858dd6ab7d9b6d7
DKFN <valere.tetelin@gmail.com>
2019-03-17 16:24:31 +0100

    Added log steps for the build

--- src/utils/RedditClient.ts
+++ src/utils/RedditClient.ts
@@ -6,7 +6,7 @@ class _RedditClient {
     private BASE_URL() {
         return process.env.NODE_ENV === 'development' || process.env.REACT_APP_APIURL
             ? "http://localhost/"
-            : process.env.REACT_APP_APIURL; // "http://reddit-backend.tetel.in/";
+            : process.env.REACT_APP_APIURL;
     }
 
     public getPost(postId: number) {
