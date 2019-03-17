ee0a9726bbc1d4da112ec5580b0db0516a40b1d2
Theo Deshayes <theo.deshayes@epita.fr>
2019-03-17 18:28:02 +0100

    cypress : add working docker

--- cypress/cypress/integration/input-form.spec.js
+++ cypress/cypress/integration/input-form.spec.js
@@ -1,5 +1,5 @@
 describe('Input form', () =>{
     it('focus on load', () => {
-        cy.visit("http://172.17.0.4:3000/")
+        cy.visit(Cypress.env('BASE_URL'))
     })
 })
--- cypresslaunch.sh
+++ cypresslaunch.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker build -t mycypress ./cypress
+docker build -t mycypress:latest ./cypress
+
 docker run mycypress 172.17.0.2:5000 run
+#docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY mycypress 172.17.0.2:3000 open
